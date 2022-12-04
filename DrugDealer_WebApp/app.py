8d4e2622f4b7890b92a95e11bdff5e1633cda3f9
gameyp <yossatorn.pht@student.mahidol.edu>
2022-12-04 12:56:48 +0700

    Update payment page

--- DrugDealer_WebApp/app.py
+++ DrugDealer_WebApp/app.py
@@ -4,6 +4,14 @@ from flask_mysqldb import MySQL
 import yaml
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Never push this line to github public repo"
+cred = yaml.load(open('cred.yaml'), Loader=yaml.Loader)
+app.config['MYSQL_HOST'] = cred['mysql_host']
+app.config['MYSQL_USER'] = cred['mysql_user']
+app.config['MYSQL_PASSWORD'] = cred['mysql_password']
+app.config['MYSQL_DB'] = cred['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 @app.route('/')
 def index_admin():
